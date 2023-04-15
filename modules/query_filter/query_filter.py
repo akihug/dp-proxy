10dbe8975da9c0edd1d6fe88365baa6c60f7f581
indra <indra@arzooo.com>
2023-04-15 20:41:28 +0530

    using all() instead of set intersections/subsets

--- modules/query_filter/query_filter.py
+++ modules/query_filter/query_filter.py
@@ -4,7 +4,7 @@ def filter_query(query_properties: list):
     aggs: list = []
     for item in query_properties[0].select.namedExpressions.seq:
         aggs.append(item.expression.name)
-    if set(ALLOWED_AGGS).intersection(set(aggs)):
+    if all(item in ALLOWED_AGGS for item in aggs):
         return True
     else:
         error = f'Only {",".join(ALLOWED_AGGS)} aggregate queries are allowed!'
